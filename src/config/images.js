f9c1c9fc318d63353feac51b1bc4fb00d1bd8d68
abhishekh-bala <cainemvhzcabhi@gmail.com>
2024-12-17 05:07:04 +0530

    Update images.js

--- src/config/images.js
+++ src/config/images.js
@@ -1,11 +1,11 @@
 // Configuration for bike images
 export const bikeImages = {
   left: {
-    url: "https://images.unsplash.com/photo-1558981806-ec527fa84c39?w=400",
+    url: "/src/config/pulsar.jpg",
     alt: "Racing Bike Left"
   },
   right: {
-    url: "https://images.unsplash.com/photo-1568772585407-9361f9bf3a87?w=400",
+    url: "/src/config/tvs-jupiter.jpg",
     alt: "Racing Bike Right"
   }
 };
