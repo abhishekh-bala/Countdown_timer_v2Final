36aa29c26f45061361661ccd40ba63b90837afe3
abhishekh-bala <cainemvhzcabhi@gmail.com>
2024-12-17 05:13:14 +0530

    Update audio.js

--- src/config/audio.js
+++ src/config/audio.js
@@ -1,13 +1,13 @@
 // Configuration for background music
 export const audioConfig = {
   defaultVideo: {
-    videoId: "jfKfPfyJRdk",
+    videoId: "9DlD7-TykIU",
     duration: 204 // 3:24 in seconds
   },
   specialVideo: {
-    videoId: "afFD-0TmIGg",
-    duration: 202, // 3:22 in seconds
-    startTime: new Date('2024-12-17T00:06:00') // 8:26 PM
+    videoId: "fvUW0Po9Fcc",
+    duration: 300, // 3:22 in seconds
+    startTime: new Date('2024-12-17T20:27:00') // 8:26 PM
     // startTime: new Date('2024-12-17T20:26:00') // 8:26 PM
   },
   options: {
